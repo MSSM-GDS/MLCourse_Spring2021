f818ba4b09e8d8ea00f8356897a89d05d7156bf2
Pushkala Jayaraman <jayaramanp@Pushkalas-MacBook-Pro.local>
2021-03-31 22:38:19 -0400

    modified existing scripts added new python scripts for MLP classifier and update of the non liner kernel.

--- ML2_Q5b.py
+++ ML2_Q5b.py
@@ -5,11 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import style
 plt.style.use('ggplot')
+
 from sklearn import svm, metrics, model_selection, preprocessing
-from sklearn.pipeline import make_pipeline
-from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, plot_precision_recall_curve, plot_roc_curve
+from sklearn.metrics import classification_report, confusion_matrix, plot_precision_recall_curve, plot_roc_curve, roc_auc_score
+from sklearn.model_selection import GridSearchCV
 from sklearn_pandas import DataFrameMapper
+from sklearn.pipeline import make_pipeline
+
 import seaborn as sns
+
 
 
 
@@ -40,9 +44,6 @@ X_train = wdbc_train.drop(['diagnosis'], axis=1)
 print(X_train.head(5))
 print(X_train.shape)
 print(X_train.describe())
-#X_train.plot()
-
-
 
 Y_train = wdbc_train['diagnosis']
 print(Y_train.head(5))
@@ -59,11 +60,7 @@ Y_test = wdbc_test['diagnosis']
 print(Y_test.head(5))
 print(Y_test.shape)
 
-
-##normalize trainig data using StandardScaler
-# X_train_min = X_train.min()
-# X_train_range = (X_train - X_train_min).max()
-# X_train_scaled = (X_train - X_train_min)/X_train_range
+##scaling so that me
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_train_scaled_features = scaler.transform(X_train)
 X_train_scaled = pd.DataFrame(X_train_scaled_features, index=X_train.index, columns=X_train.columns)
@@ -71,54 +68,31 @@ print(X_train_scaled.shape)
 print(X_train_scaled)
 
 
-##normalize test data
-# X_test_min = X_test.min()
-# X_test_range = (X_test - X_test_min).max()
-# X_test_scaled = (X_test - X_test_min)/X_test_range
 X_test_scaled_features = scaler.transform (X_test)
 X_test_scaled = pd.DataFrame(X_test_scaled_features, index=X_test.index, columns=X_test.columns)
 print(X_test_scaled.shape)
 print(X_test_scaled)
 
-# ##model fitting
-# print("Model fitting pre-norm")
-# svc_model = svm.SVC()
-# svc_model.fit(X_train, Y_train)
-# Y_predict = svc_model.predict(X_test)
-# cm = np.array(confusion_matrix(Y_test, Y_predict, labels=[1,0]))
-# confusion = pd.DataFrame(cm, index=['is_cancer', 'is_benign'],
-#                          columns=['predicted_cancer','predicted_benign'])
-# print(confusion)
-# heatmap_confusion = sns.heatmap(confusion, annot = True, linewidths=.5)
-# fig = heatmap_confusion.get_figure()
-# fig.savefig("heatmap_Confusion.png")
-# print(classification_report(Y_test,Y_predict))
-#
-# # plot ROC AUC prenorm
-# metrics.plot_roc_curve(svc_model, X_test, Y_test)
-# plt.show()
-
 ###model fitting postnorm: tried both linear models.
 print("Model fitting postnorm")
-svc_model_postnorm = svm.SVC(kernel='linear', C = 1.0)
-#svc_model_postnorm = svm.SVC(kernel='linear')
-#svc_model_postnorm = svm.LinearSVC()
-svc_model_postnorm.fit(X_train_scaled, Y_train)
-print("score:")
-print(svc_model_postnorm.score(X_test_scaled, Y_test))
-
-Y_predict_postnorm = svc_model_postnorm.predict(X_test_scaled)
-cm_postnorm = confusion_matrix(Y_test, Y_predict_postnorm)
+param_grid = {'C': [0.1, 1, 2, 3, 5, 10, 100], 'gamma': [1, 0.1, 0.05, 0.03, 0.01, 0.005, 0.001], 'kernel': ['rbf']}
+grid = GridSearchCV(svm.SVC(),param_grid,refit=True,verbose=4, cv=5)
+search = grid.fit(X_train_scaled,Y_train)
+print("Best Params:")
+print(search.best_params_)
+print(search.best_estimator_)
+gridsearch_predictions = search.predict(X_test_scaled)
+cm_postnorm = confusion_matrix(Y_test, gridsearch_predictions)
 confusion_postnorm = pd.DataFrame(cm_postnorm, index=['is_cancer', 'is_benign'],
                          columns=['predicted_cancer','predicted_benign'])
 print(confusion_postnorm)
 heatmap_confusion_postnorm = sns.heatmap(confusion_postnorm, annot = True, linewidths=.5)
 fig = heatmap_confusion_postnorm.get_figure()
-fig.savefig("heatmap_Confusion_postnorm.png")
-print(classification_report(Y_test,Y_predict_postnorm))
+fig.savefig("heatmap_Q5b_Confusion_GridSearchCV_postnorm.png")
+print(classification_report(Y_test,gridsearch_predictions))
 
 #plot ROC AUC post norm
-metrics.plot_roc_curve(svc_model_postnorm, X_test_scaled, Y_test)
+metrics.plot_roc_curve(grid, X_test_scaled, Y_test)
 plt.show()
-ROC = roc_auc_score(Y_test, svc_model_postnorm.decision_function(X_test_scaled))
+ROC = roc_auc_score(Y_test, grid.decision_function(X_test_scaled))
 print(ROC)
